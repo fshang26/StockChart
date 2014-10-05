8bad4e04e932574c7238d6fd3071de5be4eb0461
Feng Shang <feng.shang@outlook.com>
2014-10-04 20:23:48 -0700

    Authorization Part 2

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,18 @@
-var auth = require('./auth');
+var auth = require('./auth'),
+    mongoose = require('mongoose'),
+    User = mongoose.model('User');
 
 module.exports = function(app) {
   app.get('/partials/*', function(req, res) {
     res.render('../../public/app/' + req.params[0]);
   });
 
+  app.get('/api/users', auth.requiresRole('admin'), function(req, res) {
+    User.find({}).exec(function(err, collection) {
+      res.send(collection);
+    })
+  });
+  
   app.post('/login', auth.authenticate);
 
   app.post('/logout', function(req, res) {
@@ -17,5 +25,4 @@ module.exports = function(app) {
       bootstrappedUser: req.user
     });
   });
-
 }
