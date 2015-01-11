d56dd60fdc5ee0f8e79f7eae74c1144186211875
Feng Shang <feng.shang@samsung.com>
2015-01-11 12:06:24 -0800

    Final fix on first bar sometimes not drawing when scrolling

--- public/app/charts/mvChartListCtrl.js
+++ public/app/charts/mvChartListCtrl.js
@@ -127,13 +127,11 @@ angular.module('app').controller('mvChartListCtrl', function($scope, $http) {
 angular.module('app').directive("xscroll", function () {
     return function(scope, element, attrs) {
         angular.element('.chart-container').bind("scroll", function() {
-          var index =  Math.ceil((angular.element('.ohclbars').width() - angular.element('.chart-container').width() - 30 - this.scrollLeft)/10);
-          if (index >= 0) {
-            var scope = angular.element(this).scope();
-            scope.$apply(function() {
-              scope.startXIndex = index;
-            });
-          }
+          var index =  Math.ceil((angular.element('.ohclbars').width() - angular.element('.chart-container').width() - 30 - this.scrollLeft)/10),
+              scope = angular.element(this).scope();
+          scope.$apply(function() {
+            scope.startXIndex = index < 0 ? 0: index;
+          });
         });
     };
 });
